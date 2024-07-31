a47f99b8f5fe6e05d91c445d28b0ca2c15326beb
Walner <condemartinezwalner@gmail.com>
2024-07-31 13:59:21 -0600

    Agregue las entradas de material ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,40 @@
+import Autocompletar from "./components/Autocompletar"
+import Button from "./components/Button"
+import Grupobotones from "./components/Grupobotones"
+import Caja from "./components/Caja"
+import Flotante from './components/Flotante'
+import Radio from './components/Radio'
+import Clasificacion from './components/Clasificacion'
+import Seleccionar from "./components/Seleccionar"
+import Deslizante from './components/Deslizante'
+import Cambiar from './components/Cambiar'
+import Campotexto from './components/Campotexto'
+import Transferencia from './components/Transferencia'
+import Activacion from './components/Activacion'
 
 function App() {
-  return (
-    <>
-       <h1>Que hay primo Messi o Ronaldo</h1>
-       <h2>ahhhh yo digo que messi </h2>
-    </>
+  return ( 
+    <div>
+      <Autocompletar/>
+      <Button/>
+      <Grupobotones/>
+      <Caja/>
+      <Flotante/>
+      <Radio/>
+      <Clasificacion/>
+      <Seleccionar/>
+      <Deslizante/>
+      <Cambiar/>
+      <Campotexto/>
+      <Transferencia/>
+      <Activacion/>
+
+    </div>
+ 
   )
 }
 
 
 
 export default App
+
